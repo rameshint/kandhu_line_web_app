44cdbb6d84c70494a56b98f4ad5f46a313193665
Ramesh N <pramesh.cit@gmail.com>
2025-07-04 18:14:30 +0530

    changes

--- footer.php
+++ footer.php
@@ -7,11 +7,11 @@
 <!--begin::Footer-->
 <footer class="app-footer">
   <!--begin::To the end-->
-  <div class="float-end d-none d-sm-inline">Anything you want</div>
+   
   <!--end::To the end-->
   <!--begin::Copyright-->
   <strong>
-    Copyright &copy; 2014-2024&nbsp;
+    Copyright &copy; 2025 pramesh.cit@gmail.com ( Ramesh Nallamuthu )
 
   </strong>
   All rights reserved.
